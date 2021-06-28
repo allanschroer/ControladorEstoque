a338c18d4656069bad7744d12ec5e4f3a3f286f9
Allan Schroer <allanschroer@outlook.com>
2021-06-27 22:34:43 -0300

    Criado o projeto com CRUD do estoque

---
+++ ControladorEstoque/Data/ControladorEstoqueContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControladorEstoque.Models;
+
+namespace ControladorEstoque.Data
+{
+    public class ControladorEstoqueContext : DbContext
+    {
+        public ControladorEstoqueContext (DbContextOptions<ControladorEstoqueContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControladorEstoque.Models.Produto> Produto { get; set; }
+    }
+}
---
+++ ControladorEstoque/Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControladorEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public double Quantidade { get; set; }
+        
+        public Produto() { }
+        public Produto(int id, int codigo, string name, string descricao, double valor, double quantidade)
+        {
+            Id = id;
+            Codigo = codigo;
+            Name = name;
+            Descricao = descricao;
+            Valor = valor;
+            Quantidade = quantidade;
+        }
+
+
+
+    }
+}
